be18dfa635a35928e7cd07f306eac2feb1646324
Adam Poland <37116467+adammpoland@users.noreply.github.com>
2019-04-08 16:01:18 -0500

    Add files via upload

---
+++ bombV2.0/bombV2.0/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace bombV2._0
+{
+	partial class BOMB
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BOMB));
+			this.wire9 = new System.Windows.Forms.Button();
+			this.wire8 = new System.Windows.Forms.Button();
+			this.wire10 = new System.Windows.Forms.Button();
+			this.wire11 = new System.Windows.Forms.Button();
+			this.wire12 = new System.Windows.Forms.Button();
+			this.Timer = new System.Windows.Forms.Label();
+			this.wire7 = new System.Windows.Forms.Button();
+			this.output = new System.Windows.Forms.Label();
+			this.wire6 = new System.Windows.Forms.Button();
+			this.wire5 = new System.Windows.Forms.Button();
+			this.wire4 = new System.Windows.Forms.Button();
+			this.wire3 = new System.Windows.Forms.Button();
+			this.wire2 = new System.Windows.Forms.Button();
+			this.wire1 = new System.Windows.Forms.Button();
+			this.Timer2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// wire9
+			// 
+			resources.ApplyResources(this.wire9, "wire9");
+			this.wire9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.wire9.Name = "wire9";
+			this.wire9.UseVisualStyleBackColor = false;
+			this.wire9.Click += new System.EventHandler(this.wire9_Click);
+			// 
+			// wire8
+			// 
+			resources.ApplyResources(this.wire8, "wire8");
+			this.wire8.BackColor = System.Drawing.Color.Blue;
+			this.wire8.Name = "wire8";
+			this.wire8.UseVisualStyleBackColor = false;
+			this.wire8.Click += new System.EventHandler(this.wire8_Click);
+			// 
+			// wire10
+			// 
+			resources.ApplyResources(this.wire10, "wire10");
+			this.wire10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.wire10.Name = "wire10";
+			this.wire10.UseVisualStyleBackColor = false;
+			this.wire10.Click += new System.EventHandler(this.wire10_Click);
+			// 
+			// wire11
+			// 
+			resources.ApplyResources(this.wire11, "wire11");
+			this.wire11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.wire11.Name = "wire11";
+			this.wire11.UseVisualStyleBackColor = false;
+			this.wire11.Click += new System.EventHandler(this.wire11_Click);
+			// 
+			// wire12
+			// 
+			resources.ApplyResources(this.wire12, "wire12");
+			this.wire12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.wire12.Name = "wire12";
+			this.wire12.UseVisualStyleBackColor = false;
+			this.wire12.Click += new System.EventHandler(this.wire12_Click);
+			// 
+			// Timer
+			// 
+			resources.ApplyResources(this.Timer, "Timer");
+			this.Timer.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.Timer.Name = "Timer";
+			// 
+			// wire7
+			// 
+			resources.ApplyResources(this.wire7, "wire7");
+			this.wire7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.wire7.Name = "wire7";
+			this.wire7.UseVisualStyleBackColor = false;
+			this.wire7.Click += new System.EventHandler(this.wire7_Click);
+			// 
+			// output
+			// 
+			resources.ApplyResources(this.output, "output");
+			this.output.BackColor = System.Drawing.Color.Black;
+			this.output.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.output.Name = "output";
+			// 
+			// wire6
+			// 
+			resources.ApplyResources(this.wire6, "wire6");
+			this.wire6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.wire6.Name = "wire6";
+			this.wire6.UseVisualStyleBackColor = false;
+			this.wire6.Click += new System.EventHandler(this.wire6_Click);
+			// 
+			// wire5
+			// 
+			resources.ApplyResources(this.wire5, "wire5");
+			this.wire5.BackColor = System.Drawing.Color.Yellow;
+			this.wire5.Name = "wire5";
+			this.wire5.UseVisualStyleBackColor = false;
+			this.wire5.Click += new System.EventHandler(this.wire5_Click);
+			// 
+			// wire4
+			// 
+			resources.ApplyResources(this.wire4, "wire4");
+			this.wire4.BackColor = System.Drawing.Color.Blue;
+			this.wire4.Name = "wire4";
+			this.wire4.UseVisualStyleBackColor = false;
+			this.wire4.Click += new System.EventHandler(this.wire4_Click);
+			// 
+			// wire3
+			// 
+			resources.ApplyResources(this.wire3, "wire3");
+			this.wire3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.wire3.Name = "wire3";
+			this.wire3.UseVisualStyleBackColor = false;
+			this.wire3.Click += new System.EventHandler(this.wire3_Click);
+			// 
+			// wire2
+			// 
+			resources.ApplyResources(this.wire2, "wire2");
+			this.wire2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.wire2.Name = "wire2";
+			this.wire2.UseVisualStyleBackColor = false;
+			this.wire2.Click += new System.EventHandler(this.wire2_Click);
+			// 
+			// wire1
+			// 
+			resources.ApplyResources(this.wire1, "wire1");
+			this.wire1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.wire1.Name = "wire1";
+			this.wire1.UseVisualStyleBackColor = false;
+			this.wire1.Click += new System.EventHandler(this.wire1_Click);
+			// 
+			// Timer2
+			// 
+			resources.ApplyResources(this.Timer2, "Timer2");
+			this.Timer2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.Timer2.Name = "Timer2";
+			// 
+			// BOMB
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.Controls.Add(this.wire9);
+			this.Controls.Add(this.wire8);
+			this.Controls.Add(this.wire10);
+			this.Controls.Add(this.wire11);
+			this.Controls.Add(this.wire12);
+			this.Controls.Add(this.Timer);
+			this.Controls.Add(this.wire7);
+			this.Controls.Add(this.output);
+			this.Controls.Add(this.wire6);
+			this.Controls.Add(this.wire5);
+			this.Controls.Add(this.wire4);
+			this.Controls.Add(this.wire3);
+			this.Controls.Add(this.wire2);
+			this.Controls.Add(this.wire1);
+			this.Controls.Add(this.Timer2);
+			this.Name = "BOMB";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button wire9;
+		private System.Windows.Forms.Button wire8;
+		private System.Windows.Forms.Button wire10;
+		private System.Windows.Forms.Button wire11;
+		private System.Windows.Forms.Button wire12;
+		private System.Windows.Forms.Label Timer;
+		private System.Windows.Forms.Button wire7;
+		private System.Windows.Forms.Label output;
+		private System.Windows.Forms.Button wire6;
+		private System.Windows.Forms.Button wire5;
+		private System.Windows.Forms.Button wire4;
+		private System.Windows.Forms.Button wire3;
+		private System.Windows.Forms.Button wire2;
+		private System.Windows.Forms.Button wire1;
+		private System.Windows.Forms.Label Timer2;
+	}
+}
+
---
+++ bombV2.0/bombV2.0/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bombV2._0
+{
+	public partial class BOMB : Form
+	{
+		bool partial = false;
+		bool partial2 = false;
+		bool outputWire = true;
+
+		public BOMB()
+		{
+			InitializeComponent();
+			startTimer();
+		}
+
+		private async void startTimer()
+		{
+			for (int x = 59; x >= 0; x--)
+			{
+				await Task.Delay(1000);
+				if (x>10)
+				{
+					Timer.Text = "00:" + x.ToString();
+
+				}
+				if (x == 0)
+				{
+					Timer.Text = "00:00";
+					explode();
+				}
+				if (x < 10)
+				{
+					Timer.Text = "00:0" + x.ToString();
+					if (partial && partial2 && x == 0)
+						Timer.BackColor = Color.Green;
+					else
+						Timer.BackColor = Color.Red;
+				}
+
+			}
+		}
+
+		private void explode()
+		{
+			if (partial && partial2)
+			{
+				output.Text = "Somehow You did it";
+				Timer.BackColor = Color.Green;
+				Timer2.BackColor = Color.Green;
+
+			}
+			else
+			{
+				//System.Diagnostics.Process.Start("https://vignette.wikia.nocookie.net/steven-universe/images/1/13/Explosion.jpg/revision/latest?cb=20150816111347");
+				//System.Diagnostics.Process.Start("https://socioecohistory.files.wordpress.com/2012/07/nuclear-bomb-explosion.jpg");
+				//System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dOojm4TE6Ow");
+				System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=sDj72zqZakE");
+
+				output.Text = "NNNNOOOOOOOOOO";
+				Timer2.Text = Timer.Text;
+				Timer2.BackColor = Color.Red;
+				Timer.Dispose();
+			}
+		}
+
+		private void diffuse()
+		{
+			partial = true;
+			partial2 = true;
+			Timer2.Text = Timer.Text;
+			Timer2.BackColor = Color.Green;
+			Timer.Dispose();
+			//System.Threading.Thread.Sleep(10000);
+		}
+
+		private void outputText(int wire)
+		{
+			Console.WriteLine(wire);
+
+			if (outputWire)
+			{
+
+
+				switch (wire)
+				{
+					case 1:
+						output.Text = "Threat eliminated";
+						break;
+					case 2:
+						output.Text = "That's not good";
+						break;
+					case 3:
+						output.Text = "NNNNOOOOOOOOOO";
+						break;
+					case 4:
+						output.Text = "Partial Diffusion";
+						break;
+					case 5:
+						output.Text = "nothing happened";
+						break;
+					case 6:
+						output.Text = "Threat eliminated";
+						break;
+					case 7:
+						output.Text = "Partial Diffusion";
+						break;
+					case 8:
+						output.Text = "The bomb is still Active!!!";
+						break;
+					case 9:
+						output.Text = "You Saved Us!!";
+						break;
+					case 10:
+						output.Text = "I hope that was good!";
+						break;
+					case 11:
+						output.Text = "That's not good";
+						break;
+					case 12:
+						output.Text = "That's not good";
+
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		private void wire1_Click(object sender, EventArgs e)
+		{
+			wire8.Dispose();
+			outputText(1);
+			wire1.Dispose();
+		}
+
+		private void wire2_Click(object sender, EventArgs e)
+		{
+			if (partial)
+			{
+				partial = false;
+			}
+			else
+			{
+				Timer2.Text = Timer.Text;
+				Timer.Dispose();
+				explode();
+			}
+			outputText(2);
+			wire2.Dispose();
+		}
+
+		private void wire3_Click(object sender, EventArgs e)
+		{
+			explode();
+			outputText(3);
+			wire3.Dispose();
+		}
+
+		private void wire4_Click(object sender, EventArgs e)
+		{
+			partial = true;
+			outputText(4);
+			wire4.Dispose();
+		}
+
+		private void wire5_Click(object sender, EventArgs e)
+		{
+			outputText(5);
+			wire5.Dispose();
+		}
+
+		private void wire6_Click(object sender, EventArgs e)
+		{
+			wire3.Dispose();
+			outputText(6);
+			wire6.Dispose();
+		}
+
+		private void wire7_Click(object sender, EventArgs e)
+		{
+			partial2 = true;
+			outputText(7);
+			wire7.Dispose();
+		}
+
+		private void wire8_Click(object sender, EventArgs e)
+		{
+			outputText(8);
+			wire8.Dispose();
+		}
+
+		private void wire9_Click(object sender, EventArgs e)
+		{
+			outputText(9);
+			diffuse();
+			wire9.Dispose();
+		}
+
+		private void wire10_Click(object sender, EventArgs e)
+		{
+			wire4.Dispose();
+			outputText(10);
+			wire10.Dispose();
+		}
+
+		private void wire11_Click(object sender, EventArgs e)
+		{
+			//output cuts out
+			outputWire = false;
+			output.Dispose();
+			outputText(11);
+			wire11.Dispose();
+		}
+
+		private void wire12_Click(object sender, EventArgs e)
+		{
+			if (partial2)
+			{
+				partial2 = false;
+			}
+			else
+			{
+				Timer2.Text = Timer.Text;
+				Timer.Dispose();
+				explode();
+			}
+			outputText(12);
+			wire12.Dispose();
+		}
+	}
+}
